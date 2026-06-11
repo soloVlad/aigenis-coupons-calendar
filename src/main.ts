d1e6b63b4ced572e033c31ca8bae03ae257afa24
Vlad Solovey <connectwithvlad@gmail.com>
2026-06-11 22:47:19 +0300

    feat: replace logout with icon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+addIcons({ 'log-out-outline': logOutOutline });
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
